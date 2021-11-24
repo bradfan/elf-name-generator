ef1295898a80fc07e5f7b7670263556c1a43422b
Brad <brt.jst@gmail.com>
2021-11-24 11:41:54 -0700

    combine classes for css, display hidden footer

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const firstNames = ['Sparkles', 'Cookie', 'Blinky', 'Evergreen', 'Jingle', 'Elfie', 'Buddy', 'Pixie', 'Cocoa', 'Frosty', 'Mistletoe', 'Twinkle', 'Peppermint', 'Cranberry', 'Snowflake', 'Kringle', 'Snowy', 'Holly', 'Jolly', 'Sprinkles', 'Merry', 'Tipsy', 'Glitzy', 'Starlight', 'Happy', 'Tinker'];
-const middleNames = ['Glitter', 'Twinkle', 'Sugar', 'Sleigh', 'Mc', 'Icy', 'Cinnamon', 'Tinsel', 'Toffee', 'Fluffy', 'Ginger', 'Snow', 'Ninny'];
-const lastNames = ['balls', 'nose', 'uni-brow', 'pants', 'shorts', 'toes', 'drawers', 'shoes', 'bum', 'bells', 'jingles', 'nut', 'berries', 'muggins', 'hair', 'giggles', 'bottoms', 'socks',];
+const middleNames = ['Glitter', 'Twinkle', 'Sugar', 'Sleigh', "M'", 'Icy', 'Cinnamon', 'Tinsel', 'Toffee', 'Fluffy', 'Ginger', 'Snow', 'Ninny'];
+const lastNames = ['balls', 'nose', 'frown', 'pants', 'shorts', 'toes', 'drawers', 'shoes', 'bum', 'bells', 'jingles', 'nut', 'berries', 'muggins', 'hair', 'giggles', 'bottoms', 'socks',];
 const mediumContainer = document.getElementById('container');
-const hideTitle = document.querySelector('.header')
+const hideTitle = document.querySelector('.header');
+
 
 document.querySelector('.image').addEventListener('click', function() {
+  const removeTitle = document.getElementById('title');
+  removeTitle.remove()
   hideTitle.innerHTML = "";
   hideTitle.setAttribute("class", "header");
   let elfName = document.createElement('p');
@@ -14,11 +17,12 @@ document.querySelector('.image').addEventListener('click', function() {
   const randomFirstName = () => Math.trunc((Math.random() * firstNames.length));
   const randomMiddleName = () => Math.trunc((Math.random() * middleNames.length));
   const randomLastName = () => Math.trunc((Math.random() * lastNames.length));
-  elfName.innerHTML = `${firstNames[randomFirstName()]} ${middleNames[randomMiddleName()]}${lastNames[randomLastName()]}`
+  elfName.innerHTML = `"${firstNames[randomFirstName()]} ${middleNames[randomMiddleName()]}${lastNames[randomLastName()]}"`
   hideTitle.append(elfName);
-  hideTitle.setAttribute("class", "title-after");
-  const removeTitle = document.getElementById('title');
-  removeTitle.remove()
+  hideTitle.setAttribute("class", "banner");
+  let showFooter = document.getElementById('footer');
+  showFooter.classList.remove('hidden');
+  
   
 })
 
