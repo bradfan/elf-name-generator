8cbfdb1affb122cc4865a15f6d78e8a9d0889451
Brad <brt.jst@gmail.com>
2021-11-24 09:58:57 -0700

    append data to header

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 "use strict";
 
-const firstNames = ['Sparkles', 'Cookie', 'Blinky', 'Evrgreen', 'Jingle', 'Elfie', 'Buddy', 'Pixie', 'Cocoa', 'Frosty', 'Mistletoe', 'Twinkle', 'Peppermint', 'Cranberry', 'Snowflake', 'Kringle', 'Snowy', 'Holly', 'Jolly', 'Sprinkles', 'Merry', 'Tipsy', 'Glitzy', 'Starlight', 'Happy', 'Tinker'];
+const firstNames = ['Sparkles', 'Cookie', 'Blinky', 'Evergreen', 'Jingle', 'Elfie', 'Buddy', 'Pixie', 'Cocoa', 'Frosty', 'Mistletoe', 'Twinkle', 'Peppermint', 'Cranberry', 'Snowflake', 'Kringle', 'Snowy', 'Holly', 'Jolly', 'Sprinkles', 'Merry', 'Tipsy', 'Glitzy', 'Starlight', 'Happy', 'Tinker'];
 const middleNames = ['Glitter', 'Twinkle', 'Sugar', 'Sleigh', 'Mc', 'Icy', 'Cinnamon', 'Tinsel', 'Toffee', 'Fluffy', 'Ginger', 'Snow', 'Ninny'];
-const lastNames = ['balls', 'nose', 'eyes', 'pants', 'shorts', 'toes', 'drawers', 'shoes', 'bum', 'bells', 'jingles', 'nut', 'berries', 'muggins', 'hair', 'giggles',];
+const lastNames = ['balls', 'nose', 'uni-brow', 'pants', 'shorts', 'toes', 'drawers', 'shoes', 'bum', 'bells', 'jingles', 'nut', 'berries', 'muggins', 'hair', 'giggles', 'bottoms', 'socks',];
 const mediumContainer = document.getElementById('container');
-
-
-
-
+const hideTitle = document.querySelector('.header')
 
 document.querySelector('.image').addEventListener('click', function() {
-  mediumContainer.innerHTML = "";
-  let smallContainer = document.createElement('div');
-  smallContainer.id = 'small-container';
-  mediumContainer.append(smallContainer);
+  hideTitle.innerHTML = "";
+  hideTitle.setAttribute("class", "header");
   let elfName = document.createElement('p');
-  elfName.class='elf-name';
+  elfName.setAttribute('class', 'elf-name');
   const randomFirstName = () => Math.trunc((Math.random() * firstNames.length));
   const randomMiddleName = () => Math.trunc((Math.random() * middleNames.length));
   const randomLastName = () => Math.trunc((Math.random() * lastNames.length));
-  console.log(randomFirstName(), randomMiddleName(), randomLastName());
   elfName.innerHTML = `${firstNames[randomFirstName()]} ${middleNames[randomMiddleName()]}${lastNames[randomLastName()]}`
-  smallContainer.append(elfName);
-  console.log(elfName);
+  hideTitle.append(elfName);
+  hideTitle.setAttribute("class", "title-after");
+  const removeTitle = document.getElementById('title');
+  removeTitle.remove()
   
 })
+
+
+
+
